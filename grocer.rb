96735a76d081d3b8f8ce1c25d80c4f69470b3039
adamesh <adameshh@gmail.com>
2017-06-22 23:46:32 -0400

    fixed the first, stuck on coupons

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,54 @@
+require 'pry'
+
+# [
+#   {"AVOCADO"=>
+      # {:price=>3.0, :clearance=>true}},
+#    {"AVOCADO"=>
+      # {:price=>3.0, :clearance=>true}},
+#    {"KALE"=>
+      # {:price=>3.0, :clearance=>false}}
+# ]
+
+
 def consolidate_cart(cart)
-  # code here
+  # adds a count of one to each item when there are no duplicates
+  # Grocer #consolidate_cart increments count when there are multiple items
+  new_cart = {}
+  cart.each do |item|
+    grocery = item.keys.first
+    if new_cart.keys.include? grocery
+      new_cart[grocery][:count] += 1
+    else
+      new_cart[grocery] = item.values.first
+      new_cart[grocery][:count] = 1
+    end
+  end
+  new_cart
 end
 
+
+
 def apply_coupons(cart, coupons)
-  # code here
+  new_cart = {}
+  coupons.each do |coupon|
+    cart.each do |item, values|
+      if coupon[:item] == item
+        coupon_item_count = values[:count] / coupon[:num]
+        new_item_count = values[:count] % coupon[:num]
+        new_cart["#{item} W/COUPON"] = {:price => coupon[:cost],
+                                    :clearance => values[:clearance],
+                                    :count => coupon_item_count}
+        if new_item_count != 0
+          new_cart[item] = {:price => values[:price],
+                            :clearance => values[:clearance],
+                            :count => new_item_count}
+        end
+      else
+        new_cart[item] = values
+      end
+    end
+  end
+  new_cart
 end
 
 def apply_clearance(cart)
